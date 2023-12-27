fde263e6220f47ad459ddc1d056c8d19560abe20
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2023-12-27 17:31:06 -0600

    .

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,1 +1,16 @@
 'use strict';
+
+import { storage } from '@extend-chrome/storage'
+
+(async function () {
+    var haloClientId = await storage.local.get('haloClientId').then(({ haloClientId }) => {
+        return haloClientId;
+    });
+    
+    var haloClientSecret = await storage.local.get('haloClientSecret').then(({ haloClientSecret }) => {
+        return haloClientSecret;
+    });
+
+    console.log('test');
+})();
+
--- src/popup.ts
+++ src/popup.ts
@@ -5,5 +5,27 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './popup.css'
 
 (function () {
-  
+    storage.local.get('haloClientId').then(({ haloClientId }) => {
+        if (haloClientId) {
+            var input = document.getElementById('halo-client-id') as HTMLInputElement;
+            input.value = haloClientId;
+        }
+    });
+
+    storage.local.get('haloClientSecret').then(({ haloClientSecret }) => {
+        if (haloClientSecret) {
+            var input = document.getElementById('halo-client-secret') as HTMLInputElement;
+            input.value = haloClientSecret;
+        }
+    });
+
+    document.getElementById('halo-client-id')?.addEventListener('input', () => {
+        var input = document.getElementById('halo-client-id') as HTMLInputElement;
+        storage.local.set({ haloClientId: input.value });
+    });
+
+    document.getElementById('halo-client-secret')?.addEventListener('input', () => {
+        var input = document.getElementById('halo-client-secret') as HTMLInputElement;
+        storage.local.set({ haloClientSecret: input.value });
+    });
 })();
